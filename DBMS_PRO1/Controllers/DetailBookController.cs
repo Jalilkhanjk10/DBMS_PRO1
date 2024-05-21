93de60bd197b5e5b2bcb38c88a9a25ecd481e526
Jalil khan <160882506+Jalilkhanjk10@users.noreply.github.com>
2024-05-21 21:39:50 +0500

    Added Detail book controller and model
    LMS

---
+++ DBMS_PRO1/Controllers/DetailBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBMS_PRO1.Controllers
+{
+    public class DetailBookController : Controller
+    {
+        // GET: DetailBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DBMS_PRO1/Models/DetailBookModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBMS_PRO1.Models
+{
+    public class DetailBookModel
+    {
+        public int Issuanceid { get; set; }
+
+
+        public string Copyid { get; set; }
+
+
+        public DateTime bookreturndate { get; set; }
+
+
+        public int Finepolicyid { get; set; }
+    }
+}
